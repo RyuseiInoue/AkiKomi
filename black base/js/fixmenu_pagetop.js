99f8ec0fbe775c0f93db2fa21bd36f34eb7413b4
ryusei <2020m033@kuas.ac.jp>
2021-04-05 08:07:47 +0900

    move data

--- black base/js/fixmenu_pagetop.js
+++ black base/js/fixmenu_pagetop.js
@@ -40,3 +40,13 @@ fm_addEvent(window, 'load', function() {
    }
  });
 });
+
+
+
+ //マウスストーカー用のdivを取得
+ const stalker = document.getElementById('stalker'); 
+
+ //上記のdivタグをマウスに追従させる処理
+ document.addEventListener('mousemove', function (e) {
+     stalker.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
+ });
